e848fb2ef6ed2a5598f16f5ab40fabe29578f52b
drahenprofi <arndt.philipp@gmx.de>
2022-01-20 15:26:22 +0100

    Implemented and corrected tests for charts

--- lib/pages/germany/widgets/bar_chart/bar_chart_container.dart
+++ lib/pages/germany/widgets/bar_chart/bar_chart_container.dart
@@ -14,33 +14,43 @@ class BarChartContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.95,
-        height: MediaQuery.of(context).size.width * 0.95 * 0.65,
-        padding: const EdgeInsets.fromLTRB(0, 10, 20, 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
+    if (data != null && data!.isNotEmpty) {
+      return Center(
+        child: Container(
+          width: MediaQuery
+              .of(context)
+              .size
+              .width * 0.95,
+          height: MediaQuery
+              .of(context)
+              .size
+              .width * 0.95 * 0.65,
+          padding: const EdgeInsets.fromLTRB(0, 10, 20, 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                title,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: BarChartContent(data: data),
+                  ))
+            ],
+          ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              title,
-              style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: BarChartContent(data: data),
-                ))
-          ],
-        ),
-      ),
-    );
+      );
+    } else {
+      return Center(child: Text("No data provided for " + title),);
+    }
   }
 }
