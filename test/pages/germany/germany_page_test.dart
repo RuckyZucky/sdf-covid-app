cffae51384df6cbea8025036c1fdbff79bc8c730
Phillip <arndt.philipp@gmx.de>
2022-01-24 14:27:10 +0100

    Update test/pages/germany/germany_page_test.dart
    Co-authored-by: RuckyZucky <phd1be2@web.de>

--- test/pages/germany/germany_page_test.dart
+++ test/pages/germany/germany_page_test.dart
@@ -39,7 +39,7 @@ void main() {
     await tester.pumpWidget(testWidget);
     await tester.pumpAndSettle();
 
-    final appBar = find.byType(CovidCharts);
-    expect(appBar, findsOneWidget);
+    final charts = find.byType(CovidCharts);
+    expect(charts, findsOneWidget);
   });
 }
